f2cfdaae1dd2eb104e730f0eac1f25a5e5d7932f
Claudemir <claudemirramosferreira@gmail.com>
2024-12-23 15:58:43 -0300

    commit initial

---
+++ src/main/java/com/service/spring_docker_compose/SpringDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.service.spring_docker_compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerComposeApplication.class, args);
+	}
+
+}
