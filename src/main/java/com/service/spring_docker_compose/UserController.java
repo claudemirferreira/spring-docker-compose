d7dcef5e40c67f7573dcc62e03c53bbdff44a9c2
Claudemir <claudemirramosferreira@gmail.com>
2024-12-23 16:13:16 -0300

    criei um endpoint para testar o swagger e incluir a dependencia web

---
+++ src/main/java/com/service/spring_docker_compose/UserController.java
@@ -0,0 +1,15 @@
+package com.service.spring_docker_compose;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String getAll(){
+        return "ok";
+    }
+}
