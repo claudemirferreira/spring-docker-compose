df5283c66e58400948285af72a1b0e0a4d927ffd
Claudemir <claudemirramosferreira@gmail.com>
2024-12-25 09:14:26 -0300

    criar producer

--- src/main/java/com/service/spring_docker_compose/kafka/producer/ProducerService.java
+++ src/main/java/com/service/spring_docker_compose/kafka/producer/ProducerService.java
@@ -1,7 +1,8 @@
-package com.service.spring_docker_compose.kafka.config;
+package com.service.spring_docker_compose.kafka.producer;
 
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,11 +11,12 @@ public class ProducerService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    private static final String TOPIC = "my-topic"; // Tópico que você criou no Kafka
+    private static final String TOPIC = "my-topic";
 
     public void sendMessage(String message) {
         kafkaTemplate.send(TOPIC, message);
         System.out.println("Mensagem enviada: " + message);
     }
+
 }
 
